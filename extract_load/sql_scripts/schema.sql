fad8a283e649f194c7fd2f4e7c71aeaa343794d4
Venkatesh <venkateshhs.shivashankar@gmail.com>
2024-05-10 11:47:37 +0200

    final query for providing Find the patient(s) with the most generated minutes

--- extract_load/sql_scripts/schema.sql
+++ extract_load/sql_scripts/schema.sql
@@ -30,3 +30,78 @@ select count(*) from exercises;
 select count(*) from patients;
 
 
+--Insert another column to store generated minutes
+ALTER TABLE steps ADD COLUMN generated_minutes FLOAT;
+
+
+
+-- just to check schema
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'steps';
+
+SELECT table_name, column_name
+FROM information_schema.columns
+WHERE table_name IN ('steps', 'exercises', 'patients');
+
+
+-- renames STEPS column to footsteps to remove ambuguity and casting errors while converting steps
+ALTER TABLE steps RENAME COLUMN "STEPS" TO footsteps;
+
+UPDATE steps SET generated_minutes = CAST(footsteps AS double precision) * 0.002;
+
+-- Intermediate Aggregate total minutes from both steps and exercises
+-- verified with excel outputs
+
+WITH StepMinutes AS (
+    SELECT "EXTERNAL_ID", SUM(generated_minutes) AS total_generated_minutes
+    FROM steps
+    GROUP BY "EXTERNAL_ID"
+),
+ExerciseMinutes AS (
+    SELECT "EXTERNAL_ID", SUM("MINUTES") AS total_exercise_minutes
+    FROM exercises
+    GROUP BY "EXTERNAL_ID"
+)
+SELECT
+    s."EXTERNAL_ID",
+    COALESCE(s.total_generated_minutes, 0) + COALESCE(e.total_exercise_minutes, 0) AS total_minutes
+FROM
+    StepMinutes s
+FULL OUTER JOIN ExerciseMinutes e
+ON s."EXTERNAL_ID" = e."EXTERNAL_ID" ORDER BY s."EXTERNAL_ID" DESC;
+
+
+
+-- COrrect Version wirth patient details
+WITH StepMinutes AS (
+    SELECT "EXTERNAL_ID", SUM("generated_minutes") AS total_generated_minutes
+    FROM steps
+    GROUP BY "EXTERNAL_ID"
+),
+ExerciseMinutes AS (
+    SELECT "EXTERNAL_ID", SUM("MINUTES") AS total_exercise_minutes
+    FROM exercises
+    GROUP BY "EXTERNAL_ID"
+),
+CombinedMinutes AS (
+    SELECT
+        COALESCE(s."EXTERNAL_ID", e."EXTERNAL_ID") AS "EXTERNAL_ID",
+        COALESCE(s.total_generated_minutes, 0) + COALESCE(e.total_exercise_minutes, 0) AS total_minutes
+    FROM
+        StepMinutes s
+    FULL OUTER JOIN ExerciseMinutes e
+    ON s."EXTERNAL_ID" = e."EXTERNAL_ID"
+)
+SELECT
+    p."PATIENT_ID",
+    p."first_name",
+    p."last_name",
+    p."country",
+    cm.total_minutes
+FROM
+    CombinedMinutes cm
+JOIN patients p ON cm."EXTERNAL_ID" = p."PATIENT_ID"
+ORDER BY cm.total_minutes DESC;
+
+
